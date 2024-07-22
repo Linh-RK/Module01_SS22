1c982024b26a7584522b7a093a4ba6bc33e320ad
Linh Linh <pttlinh101193@gmail.com>
2024-07-22 09:39:29 +0700

    Add files via upload

--- Categories/categories.js
+++ Categories/categories.js
@@ -7,7 +7,9 @@ console.log(add);
 const inputCate = document.getElementById("inputCate");
 console.log(inputCate);
 // lay gia tri trong in put ra.value
-
+// const
+let inputGlobal = "";
+let h2 = document.querySelector("h2");
 btnAdd.addEventListener("click", () => {
   const dbCate = JSON.parse(localStorage.getItem("categories")) || [];
   const cateName = inputCate.value.trim();
@@ -25,19 +27,35 @@ btnAdd.addEventListener("click", () => {
     alert("Ten bi trung");
     return;
   }
+  console.log(!inputGlobal);
   //   tao obj cho phan tu moi
-  let id = 1;
-  if (dbCate.length > 0) {
-    id = dbCate[dbCate.length - 1].id + 1;
+  if (!inputGlobal) {
+    let id = 1;
+    if (dbCate.length > 0) {
+      id = dbCate[dbCate.length - 1].id + 1;
+    }
+    const newCate = {
+      id: id,
+      name: cateName,
+    };
+    dbCate.push(newCate);
+    window.localStorage.setItem("categories", JSON.stringify(dbCate));
+    inputCate.value = "";
+    renderCate();
+  } else {
+    const dbCate = JSON.parse(localStorage.getItem("categories")) || [];
+    const inputGlobal = JSON.parse(localStorage.getItem("inputGlobal")) || [];
+    console.log(inputGlobal);
+    let findIndex = dbCate.findIndex((cate) => cate.id == inputGlobal);
+    console.log(inputCate.value);
+    console.log(findIndex);
+    dbCate[findIndex].name = inputCate.value;
+    console.log(dbCate);
+    window.localStorage.setItem("categories", JSON.stringify(dbCate));
+    // inputGlobal = "";
+    h2.innerHTML = "THÊM DANH MỤC";
+    renderCate();
   }
-  const newCate = {
-    id: id,
-    name: cateName,
-  };
-  dbCate.push(newCate);
-  window.localStorage.setItem("categories", JSON.stringify(dbCate));
-  inputCate.value = "";
-  renderCate();
 });
 
 const lineCate = document.getElementById("lineCate");
@@ -53,14 +71,35 @@ function renderCate() {
          <td>${dbCate[i].id}</td>
          <td id="nameCate">${dbCate[i].name}</td>
          <td>
-         <button>Sửa</button>
-         <button>Xoá</button>
+         <button onclick = "update(${dbCate[i].id})">Sửa</button>
+         <button onclick="deleteF(${dbCate[i].id})">Xoá</button>
          </td>
       </tr>
     `;
   }
   // + chuoi
   lineCate.innerHTML = stringHTML;
+
   // inner HTML
 }
 renderCate();
+
+function deleteF(id) {
+  const dbCate = JSON.parse(localStorage.getItem("categories")) || [];
+  let findIndex = dbCate.findIndex((cate) => cate.id == id);
+  console.log(findIndex);
+  console.log(id);
+  dbCate.splice(findIndex, 1);
+  window.localStorage.setItem("categories", JSON.stringify(dbCate));
+  renderCate();
+}
+function update(id) {
+  const dbCate = JSON.parse(localStorage.getItem("categories")) || [];
+  let findIndex = dbCate.findIndex((cate) => cate.id == id);
+  console.log(findIndex);
+  inputCate.value = dbCate[findIndex].name;
+  // console.log(inputCate.value);
+  h2.innerHTML = "SỬA DANH MỤC";
+  inputGlobal = dbCate[findIndex].id;
+  window.localStorage.setItem("inputGlobal", JSON.stringify(inputGlobal));
+}
